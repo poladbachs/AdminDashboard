a96e6e0b076aaaad484d6fefaa8f74dd6d71afa4
Polad <bakhip@usi.ch>
2024-10-08 00:01:58 +0200

    createroot setup

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import { StrictMode } from "react";
+import { createRoot } from "react";
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        < App/>
+    </StrictMode>
+)
