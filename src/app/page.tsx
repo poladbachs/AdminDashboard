e8d345bff10dc5abb5f3e5571592fe5b1c69ecfb
Polad <bakhip@usi.ch>
2024-10-08 14:48:14 +0200

    starting to style main table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,70 +7,61 @@ import { useRouter, usePathname } from 'next/navigation';
 const drawerWidth = 240;
 
 export default function Home() {
-  const router = useRouter();
-  const pathname = usePathname();
-  const [selectedModule, setSelectedModule] = useState<string>('Django');
+    const router = useRouter();
+    const pathname = usePathname();
+    const [selectedModule, setSelectedModule] = useState<string>('Django');
 
-  useEffect(() => {
-    const pathModule = pathname === '/' ? 'Django' : pathname.substring(1).charAt(0).toUpperCase() + pathname.substring(2);
-    setSelectedModule(pathModule);
-  }, [pathname]);
+    useEffect(() => {
+        const pathModule = pathname === '/' ? 'Django' : pathname.substring(1).charAt(0).toUpperCase() + pathname.substring(2);
+        setSelectedModule(pathModule);
+    }, [pathname]);
 
-  const handleModuleClick = (module: string) => {
-    setSelectedModule(module);
-    router.push(`/${module.toLowerCase()}`);
-  };
+    const handleModuleClick = (module: string) => {
+        setSelectedModule(module);
+        router.push(`/${module.toLowerCase()}`);
+    };
 
-  return (
-    <Box className="flex">
-      <CssBaseline />
+    return (
+        <Box className="flex">
+            <CssBaseline />
 
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="permanent"
-        anchor="left"
-      >
-        <Toolbar className="justify-center">
-          <Image src="https://dec-energy.ch/safari-pinned-tab.svg" alt="DEC Logo" width={120} height={40} />
-        </Toolbar>
-        <List className="ml-4">
-          {['Django', 'Website', 'Database'].map((text) => (
-            <ListItem
-              component="button"
-              key={text}
-              onClick={() => handleModuleClick(text)}
-              className={`space-x-3 rounded-l-lg rounded-r-none ${selectedModule === text ? 'bg-[#F0F0F0]' : ''} hover:bg-[#E0E0E0]`}
-              >
-              <ListItemIcon className="min-w-0">
-                <Box className="w-5 h-5 bg-black" />
-              </ListItemIcon>
-              <ListItemText primary={text} className="flex justify-start"/>
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                    },
+                }}
+                variant="permanent"
+                anchor="left"
+            >
+                <Toolbar className="justify-center">
+                    <Image src="https://dec-energy.ch/safari-pinned-tab.svg" alt="DEC Logo" width={120} height={40} />
+                </Toolbar>
+                <List className="ml-4">
+                    {['Django', 'Website', 'Database'].map((text) => (
+                        <ListItem
+                            component="button"
+                            key={text}
+                            onClick={() => handleModuleClick(text)}
+                            className={`space-x-3 rounded-l-lg rounded-r-none ${selectedModule === text ? 'bg-[#F0F0F0]' : ''} hover:bg-[#E0E0E0]`}
+                        >
+                            <ListItemIcon className="min-w-0">
+                                <Box className="w-5 h-5 bg-black" />
+                            </ListItemIcon>
+                            <ListItemText primary={text} className="flex justify-start" />
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
 
-      <main>
-        <Grid
-          component="main"
-          sx={{ flexGrow: 1, bgcolor: '#f4f7f9', p: 3, marginLeft: `${drawerWidth}px` }}
-        >
-          <AppBar position="static" className="bg-white">
-            <Toolbar>
-              <Typography variant="h6" noWrap color="black">
-                {selectedModule}
+            <main>
+              <Typography variant="h6" color="black">
+                  {selectedModule}
               </Typography>
-            </Toolbar>
-          </AppBar>
-        </Grid>
-      </main>
-    </Box>
-  );
+            </main>
+        </Box>
+    );
 }
