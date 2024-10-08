691ffe30ff410ee3bafefc5eb8183f502b3a983e
Polad <bakhip@usi.ch>
2024-10-08 03:51:21 +0200

    styling modules in sidebar seems done, now dealing with routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
 "use client";
 import { AppBar, Toolbar, Drawer, List, ListItem, ListItemIcon, Grid, ListItemText, CssBaseline, Box, Typography } from '@mui/material';
 import Image from 'next/image';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 const drawerWidth = 200;
 
 export default function Home() {
   const router = useRouter();
+  const [selectedModule, setSelectedModule] = useState<string | null>('Django');
+
+  useEffect(() => {
+    if (!selectedModule) {
+      router.push('/django');
+    }
+  }, [selectedModule, router]);
+
+  const handleModuleClick = (module: string) => {
+    setSelectedModule(module);
+    router.push(`/${module.toLowerCase()}`);
+  };
 
   return (
     <Box className="flex">
@@ -16,11 +28,9 @@ export default function Home() {
       <Drawer
         sx={{
           width: drawerWidth,
-          flexShrink: 0,
           '& .MuiDrawer-paper': {
             width: drawerWidth,
             boxSizing: 'border-box',
-            backgroundColor: 'white',
           },
         }}
         variant="permanent"
@@ -29,13 +39,18 @@ export default function Home() {
         <Toolbar className="justify-center">
           <Image src="https://dec-energy.ch/safari-pinned-tab.svg" alt="DEC Logo" width={120} height={40} />
         </Toolbar>
-        <List>
-          {['Django', 'Website', 'Database'].map((text, index) => (
-            <ListItem button key={text} onClick={() => router.push(`/${text.toLowerCase()}`)} className="space-x-3 pl-2">
-              <ListItemIcon className="flex justify-end">
-                <Box className="w-3 h-3 bg-black" />
+        <List className="ml-4">
+          {['Django', 'Website', 'Database'].map((text) => (
+            <ListItem
+              component="button"
+              key={text}
+              onClick={() => handleModuleClick(text)}
+              className={`space-x-3 rounded-l-lg rounded-r-none ${selectedModule === text ? 'bg-[#F0F0F0]' : ''} hover:bg-[#F0F0F0]`}
+              >
+              <ListItemIcon className="min-w-0">
+                <Box className="w-5 h-5 bg-black" />
               </ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemText primary={text} className="flex justify-start"/>
             </ListItem>
           ))}
         </List>
